ff5364810136e343c9a76e199919f8e9984294a9
Tyler-Bibus <tylerbibus@hotmail.com>
2021-04-29 16:32:17 -0500

    added the sprites

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -43,7 +43,8 @@ class StoryMenuState extends MusicBeatState
 		['pico', 'bf', 'gf'],
 		['mom', 'bf', 'gf'],
 		['parents-christmas', 'bf', 'gf'],
-		['senpai', 'bf', 'gf']
+		['senpai', 'bf', 'gf'],
+		['dad', 'bf', 'gf']
 		
 	];
 
